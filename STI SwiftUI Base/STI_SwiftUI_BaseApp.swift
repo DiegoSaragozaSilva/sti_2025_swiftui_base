40b5fd54e73454f5e8d2e4c92fb1916839392868
Diego Saragoza da Silva <diegosaragozaa2@gmail.com>
2025-09-30 10:59:04 -0300

    feat: added project

---
+++ STI SwiftUI Base/STI_SwiftUI_BaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  STI_SwiftUI_BaseApp.swift
+//  STI SwiftUI Base
+//
+//  Created by Diego Saragoza Da Silva on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct STI_SwiftUI_BaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
